12ae86300ee79bfe55ef5edb65eaa2c44c1ae48c
somsom13 <somin1113@naver.com>
2022-02-17 21:55:53 +0900

    [feat]자체 로그인 구현

---
+++ src/main/java/com/example/demo/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,78 @@
+package com.example.demo.auth;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final AuthenticationManager authManager;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserDetailsServiceImpl(UserRepository userRepository,AuthenticationManager authManager,BCryptPasswordEncoder bCryptPasswordEncoder) {
+
+        this.userRepository = userRepository;
+        this.authManager=authManager;
+        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+    }
+
+    public String checkEmailValidate(String email){
+        User user=userRepository.findByUserEmail(email);
+        if(user!=null){
+            return "email conflict";
+        }else{
+            return "email validate";
+        }
+    }
+
+    public String checkNicknameValidate(String nickname){
+        User user=userRepository.findByUserNickname(nickname);
+        if(user!=null){
+            return "nickname conflict";
+        }else{
+            return "nickname validate";
+        }
+    }
+
+    public Long saveUser(User user){
+        return userRepository.save(user).getUserId();
+    }
+
+
+    public Authentication verifyLoginInfo(String email, String pwd){
+        User user=userRepository.findByUserEmail(email);
+        if(user==null){
+            return null;
+        }
+        if(!bCryptPasswordEncoder.matches(pwd,user.getUserPassword())){
+            //전달 파라미터가 암호화 되지 않은 비밀번호
+            return null;
+        }
+
+        //Authentication Token 생성 (username, password) 사용
+        //여기서 username: 중복되지 않는 고유값 -> email로 대체하여 사용
+        UsernamePasswordAuthenticationToken authToken=new UsernamePasswordAuthenticationToken(email,pwd);
+        return authManager.authenticate(authToken);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        //spring security에서 로그인 된 사용자의 정보를 이 메소드를 통해 가져와, security context에 저장한다!
+        User user=userRepository.findByUserEmail(email);
+        return new CustomUserDetails(user);
+        //CustomUserDetails는 Authentication type으로 생각하는게 이해하기 쉬움
+        //User Entity 정보와 authority 등 부가 정보 함께 가짐
+    }
+}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,25 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.dataObject.vo.SigninVO;
 import com.example.demo.dataObject.vo.SignupVO;
 import com.example.demo.domain.User;
 import com.example.demo.dataObject.dto.ResponseMessage;
-import com.example.demo.service.UserManagementService;
+import com.example.demo.auth.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.security.Security;
+import java.util.Collection;
 
 @RestController
 public class UserController {
 
-    private UserManagementService userService;
+    private UserDetailsServiceImpl userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserController(UserManagementService userService, BCryptPasswordEncoder bCryptPasswordEncoder){
+    public UserController(UserDetailsServiceImpl userService, BCryptPasswordEncoder bCryptPasswordEncoder){
         this.userService=userService;
         this.bCryptPasswordEncoder=bCryptPasswordEncoder;
     }
@@ -34,9 +39,6 @@ public class UserController {
         String pwd= signupVO.getPassword();
         String encodedPwd=bCryptPasswordEncoder.encode(pwd);
         String imgUrl=signupVO.getImageUrl();
-        System.out.println("imgUrl = " + imgUrl);
-
-
 
         User user= new User(signupVO.getName(),signupVO.getNickname(),signupVO.getEmail(),encodedPwd);
         if(imgUrl!=null){
@@ -53,7 +55,7 @@ public class UserController {
         if(valid.equals("email validate")){
             return new ResponseEntity<>(new ResponseMessage(200,"이메일 사용가능"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ResponseMessage(409,"이메일 중복"), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(new ResponseMessage(409,"이미 사용중인 이메일"), HttpStatus.CONFLICT);
     }
 
     @GetMapping("/signup/{nickname}")
@@ -62,12 +64,39 @@ public class UserController {
         if(valid.equals("nickname validate")){
             return new ResponseEntity<>(new ResponseMessage(200,"닉네임 사용가능"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ResponseMessage(409,"닉네임 중복"), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(new ResponseMessage(409,"이미 사용중인 닉네임"), HttpStatus.CONFLICT);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<ResponseMessage> signin(@RequestBody SigninVO signinVO){
+        String email=signinVO.getEmail();
+        String password=signinVO.getPassword();
+        Authentication auth=userService.verifyLoginInfo(email,password);
+//        System.out.println("authentication: "+auth.getPrincipal());
+        if(auth!=null){
+            //login success
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            Object principal=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            //SecurityContext에 Authentication 정보 저장
+            return new ResponseEntity<>(ResponseMessage.withData(200, "로그인 성공", principal),HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new ResponseMessage(401,"로그인 실패"),HttpStatus.UNAUTHORIZED);
     }
 
 
     @GetMapping("/")
     public String landing(){
     return "landing page";
+    }
+
+    @GetMapping("/review")
+    public String test(){
+        //로그인 상태 유지 확인 테스트 성공
+        Object principal=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails=(UserDetails) principal;
+        String username = userDetails.getUsername();
+        System.out.println("username = " + username);
+        return "success";
     }
 }
