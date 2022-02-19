24452fadec41b1c46b4228d6ef5298c22dee6fb6
somsom13 <somin1113@naver.com>
2022-02-20 03:44:36 +0900

    [ADD]CORS 설정

--- src/main/java/com/example/demo/auth/WebMvcConfig.java
+++ src/main/java/com/example/demo/auth/WebMvcConfig.java
@@ -2,8 +2,13 @@ package com.example.demo.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -11,10 +16,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("*")
+                .allowCredentials(true)
                 .maxAge(3000);
+
     }
+
+
 }
