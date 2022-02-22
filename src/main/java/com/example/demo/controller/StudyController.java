ac0fd36814060b526237bddd77679098d1a97268
somsom13 <somin1113@naver.com>
2022-02-22 20:24:18 +0900

    [FEAT]스터디글 신고 API

--- src/main/java/com/example/demo/controller/StudyController.java
+++ src/main/java/com/example/demo/controller/StudyController.java
@@ -7,8 +7,6 @@ import com.example.demo.dto.StudyPostDTO;
 import com.example.demo.security.UserDetailsServiceImpl;
 import com.example.demo.service.StudyPostService;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,38 +41,55 @@ public class StudyController {
         return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글이 등록 되었습니다.",newPost), HttpStatus.OK);
     }
 
-    @GetMapping("/studies/{id}") // 스터디 댓글 기능, 엔티티 생성하고 나면 이것도 연결하기
-    public ResponseEntity<ResponseMessage> viewStudyPost(@PathVariable Long id){
-        StudyPost post=studyPostService.findStudyPostById(id);
+    @GetMapping("/studies/{studyId}") // 스터디 댓글 기능, 엔티티 생성하고 나면 이것도 연결하기
+    public ResponseEntity<ResponseMessage> viewStudyPost(@PathVariable Long studyId){
+        StudyPost post=studyPostService.findStudyPostById(studyId);
         //댓글데이터까지 함꼐 ResponseMessage에 넣어서 반환해주기
-        if(post!=null){
+        if(post!=null&&post.getStudyStatus()==1){
             return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글 찾음",post),HttpStatus.OK);
         }
         return new ResponseEntity<>(new ResponseMessage(400,"존재하지 않는 스터디글 요청"),HttpStatus.OK);
     }
 
-    @PatchMapping("/studies/{id}")
-    public ResponseEntity<ResponseMessage> modifyPost(@PathVariable Long id, @RequestBody StudyPostDTO postDTO){
-        //스터디글에서 수정 가능한것: 제목, 내용, 카테고리, 위치, 최소인원, 최대인원 - >DTO에서 userId만 null로 받아오고 바꾸면 될듯?
-        //그러면 파라미터로 넘어온 id를 통해서 Post찾고, update 쿼리문을 써서 바꿔주기
-        //필요 부분만 업데이트 해주어야 하므로 update 쿼리 작성 ->  recruiteStatus, repostCount, user, postId, createdDate, studyStatus 제외하고 update
-        StudyPost post=studyPostService.modifyStudyPost(postDTO,id);
+    @PatchMapping("/studies/{studyId}")
+    public ResponseEntity<ResponseMessage> modifyPost(@PathVariable Long studyId, @RequestBody StudyPostDTO postDTO){
+        //recruitStatus, repostCount, user, postId, createdDate, studyStatus 제외하고 update
+        StudyPost post=studyPostService.modifyStudyPost(postDTO,studyId);
         if(post!=null){
             return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글 수정 성공",post),HttpStatus.OK);
         }else{
-            return new ResponseEntity<>(new ResponseMessage(400,"잘못된 수정 요청"),HttpStatus.OK);
+            return new ResponseEntity<>(new ResponseMessage(404,"잘못된 수정 요청"),HttpStatus.OK);
         }
     }
 
-    @DeleteMapping("/studies/{id}")
-    public ResponseEntity<ResponseMessage> deletePost(@PathVariable Long id){
-        StudyPost post=studyPostService.findStudyPostById(id);
+    @DeleteMapping("/studies/{studyId}")
+    public ResponseEntity<ResponseMessage> deletePost(@PathVariable Long studyId){
+        StudyPost post=studyPostService.findStudyPostById(studyId);
         if(post!=null){
             post.updateStudyStatus(0); // 0이면 삭제된 글
-            studyPostService.saveStudyPost(post); //삭제한 정보를 반영
-            return new ResponseEntity<>(new ResponseMessage(200,id+"번 글 삭제 성공"),HttpStatus.OK);
+            studyPostService.saveStudyPost(post); //삭제한 정보를 반영 -> 근데 삭제된 글이면 User가 가지고 있는 글 보여줄 때도 status 0인 글 제외하고 보여줄 수 있겠지?
+            return new ResponseEntity<>(new ResponseMessage(200,studyId+"번 글 삭제 성공"),HttpStatus.OK);
         }else{
-            return new ResponseEntity<>(new ResponseMessage(400,"잘못된 삭제 요청"),HttpStatus.OK);
+            return new ResponseEntity<>(new ResponseMessage(404,"잘못된 삭제 요청"),HttpStatus.OK);
         }
     }
+
+    @GetMapping("/studies/{studyId}/reports")
+    public ResponseEntity<ResponseMessage> reportPost(@PathVariable Long studyId){
+        StudyPost post=studyPostService.findStudyPostById(studyId);
+        Integer reportCount=post.getStudyReportCount();
+        post.updateStudyReposrtCount(++reportCount);
+
+//        System.out.println("reportCount = " + reportCount);
+        if(reportCount==5){
+            post.updateStudyStatus(0); // 5번 신고된 글은 삭제 처리
+            studyPostService.saveStudyPost(post);
+            return new ResponseEntity<>(new ResponseMessage(200,studyId+"번 글은 신고가 5번 누적되어 삭제되었습니다."),HttpStatus.OK);
+        }
+
+        studyPostService.saveStudyPost(post); // 신고내역 update 후 저장
+        return new ResponseEntity<>(new ResponseMessage(200,studyId+"번 글 신고 완료"),HttpStatus.OK);
+    }
+
+
 }
