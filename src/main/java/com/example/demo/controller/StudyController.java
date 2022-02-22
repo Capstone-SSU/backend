0c0a08ea22ab17887f41480ccffdb683f531110b
somsom13 <somin1113@naver.com>
2022-02-22 18:21:11 +0900

    [FEAT]스터디글 수정 API

--- src/main/java/com/example/demo/controller/StudyController.java
+++ src/main/java/com/example/demo/controller/StudyController.java
@@ -6,16 +6,20 @@ import com.example.demo.dto.ResponseMessage;
 import com.example.demo.dto.StudyPostDTO;
 import com.example.demo.security.UserDetailsServiceImpl;
 import com.example.demo.service.StudyPostService;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @RestController
+@AllArgsConstructor
+@Transactional
 public class StudyController {
 
     private final StudyPostService studyPostService;
@@ -23,42 +27,41 @@ public class StudyController {
     @PersistenceContext
     private final EntityManager em;
 
-    public StudyController(StudyPostService studyPostService, UserDetailsServiceImpl userDetailsService, EntityManager em) {
-        this.studyPostService = studyPostService;
-        this.userDetailsService = userDetailsService;
-        this.em = em;
-    }
-
 
     @PostMapping("/studies")
-    public ResponseEntity<ResponseMessage> uploadStudyPost(@RequestBody StudyPostDTO post){
+    public ResponseEntity<ResponseMessage> uploadStudyPost(@RequestBody StudyPostDTO postDto){
         //StudyPost 객체를 그대로 반환
-        String title=post.getTitle();
-        String content=post.getContent();
-        String location=post.getLocation();
-        Long writerId=post.getUserId();
-        Integer recruitStatus=post.getRecruitStatus();
-        String category=post.getCategory();
-        Integer min=post.getMinReq();
-        //maxReq는 채워져 있으면 받아옴
+        StudyPost newPost = new StudyPost(postDto);
 
-        StudyPost newPost = new StudyPost(title,content,category,location,recruitStatus,min);
-        if(post.getMaxReq()!=null){
-            newPost.setStudyMaxReq(post.getMaxReq());
-        }
-
+        Long writerId=postDto.getUserId();
         User user=userDetailsService.findUserById(writerId);
-//        System.out.println("USER: "+user);
         newPost.setUser(user); // 외래키로 연결된 User를 저장함 ->
-//        em.persist(newPost);
+        em.persist(newPost);
         studyPostService.saveStudyPost(newPost);
 
         return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글이 등록 되었습니다.",newPost), HttpStatus.OK);
     }
 
-    @GetMapping("/studies/{id}")
-    public ResponseEntity<ResponseMessage> test(@PathVariable Long id){
+    @GetMapping("/studies/{id}") // 스터디 댓글 기능, 엔티티 생성하고 나면 이것도 연결하기
+    public ResponseEntity<ResponseMessage> viewStudyPost(@PathVariable Long id){
         StudyPost post=studyPostService.findStudyPostById(id);
-        return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글 찾음",post),HttpStatus.OK);
+        //댓글데이터까지 함꼐 ResponseMessage에 넣어서 반환해주기
+        if(post!=null){
+            return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글 찾음",post),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ResponseMessage(400,"존재하지 않는 스터디글 요청"),HttpStatus.OK);
+    }
+
+    @PatchMapping("/studies/{id}")
+    public ResponseEntity<ResponseMessage> modifyPost(@PathVariable Long id, @RequestBody StudyPostDTO postDTO){
+        //스터디글에서 수정 가능한것: 제목, 내용, 카테고리, 위치, 최소인원, 최대인원 - >DTO에서 userId만 null로 받아오고 바꾸면 될듯?
+        //그러면 파라미터로 넘어온 id를 통해서 Post찾고, update 쿼리문을 써서 바꿔주기
+        //필요 부분만 업데이트 해주어야 하므로 update 쿼리 작성 ->  recruiteStatus, repostCount, user, postId, createdDate, studyStatus 제외하고 update
+        StudyPost post=studyPostService.modifyStudyPost(postDTO,id);
+        if(post!=null){
+            return new ResponseEntity<>(ResponseMessage.withData(200,"스터디글 수정 성공",post),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(new ResponseMessage(400,"잘못된 수정 요청"),HttpStatus.OK);
+        }
     }
 }
--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -57,7 +57,7 @@ public class User {
     private String githubUrlName;
 
     @OneToMany(mappedBy = "user", targetEntity = StudyPost.class) // StudyPost와의 양방향 매핑을 위해 추가, 연관관계의 주인은 studyPost entity
-    @JsonBackReference
+    @JsonManagedReference
     private List<StudyPost> studyPostList=new ArrayList<>();
 
     @Builder
