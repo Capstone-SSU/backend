62b7f74b2f4c451202c67dd2df3408b499ee5e22
somsom13 <somin1113@naver.com>
2022-02-27 00:10:12 +0900

    [FIX]로그인 성공 후 User 객체 함께 response

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,10 +1,16 @@
 package com.example.demo.dto;
 
+import com.example.demo.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AuthResponse {
     private String jwtToken;
+    UserOnlyDto user;
+
+    public AuthResponse(String jwtToken, UserOnlyDto user) {
+        this.jwtToken=jwtToken;
+        this.user=user;
+    }
 }
