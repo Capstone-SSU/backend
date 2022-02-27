23c070df79227475827bc58590e018ef63f84551
somsom13 <somin1113@naver.com>
2022-02-28 00:10:59 +0900

    [MOD]like study랑 user로 찾기

--- src/main/java/com/example/demo/like/LikeService.java
+++ src/main/java/com/example/demo/like/LikeService.java
@@ -46,17 +46,8 @@ public class LikeService {
     //studyPost와 user로 찾는게 있어야함
     public Like findLikeByStudyPostandUser(StudyPost post, User user){
         //user로 먼저 찾고 그 다음에 거기 내에서 studyPost로 찾는거
-        List<Like> interestsByUser= likeRepository.findAllLikeByUser(user);
-
-        //list에서 특정 StudyPost 한 개를 찾는 기능 다른데서도 사용되면 메소드로 따로 빼두기
-        Like like =null;
-        for (Like interest : interestsByUser) {
-            if (interest.getStudyPost() == post) {
-                like = interest;
-                break;
-            }
-        }
-        return like;
+        Optional<Like> foundLike=likeRepository.findLikeByUserAndStudyPost(user,post);
+        return foundLike.orElse(null);
     }
 
     public List<Like> findAllLikesOnPost(StudyPost post){
