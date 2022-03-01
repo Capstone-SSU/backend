06ccf9f05ecb844b12daf1cc59cd1a1831a354e6
somsom13 <somin1113@naver.com>
2022-03-02 00:28:57 +0900

    [FEAT] 로드맵 등록 위한 사용자 전체 리뷰 강의 조회

--- src/main/java/com/example/demo/roadmap/roadMapController.java
+++ src/main/java/com/example/demo/roadmap/roadMapController.java
@@ -3,18 +3,20 @@ package com.example.demo.roadmap;
 import com.example.demo.dto.ResponseMessage;
 import com.example.demo.lecture.Lecture;
 import com.example.demo.lecture.LectureService;
+import com.example.demo.review.Review;
+import com.example.demo.review.ReviewService;
 import com.example.demo.roadmap.dto.RoadMapDto;
+import com.example.demo.roadmap.dto.RoadmapLecturesResponse;
 import com.example.demo.user.User;
 import com.example.demo.user.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,8 +25,9 @@ public class roadMapController {
     private final RoadMapService roadMapService;
     private final UserDetailsServiceImpl userDetailsService;
     private final LectureService lectureService;
+    private final ReviewService reviewService;
 
-    @PostMapping("/roadmaps")
+    @PostMapping("/roadmaps") //넘어온 lectures가 null이거나 empty면 다른 response 만들기
     public ResponseEntity<ResponseMessage> uploadRoadmap(@RequestBody RoadMapDto roadMapDto, Principal principal){
         User user=userDetailsService.findUserByEmail(principal.getName());
         String title=roadMapDto.getRoadmapTitle();
@@ -44,6 +47,28 @@ public class roadMapController {
             roadMapService.saveRoadmap(roadMap);
         }
         return new ResponseEntity<>(new ResponseMessage(201,"새로운 로드맵 등록 성공"),HttpStatus.OK);
+    }
+
+    @GetMapping("/roadmaps/{userId}")
+    public ResponseEntity<ResponseMessage> getAllLecturesForRoadmap(@PathVariable Long userId){
+        User user=userDetailsService.findUserById(userId);
+        if(user==null){
+            return new ResponseEntity<>(new ResponseMessage(404,"존재하지 않는 사용자에 대한 요청입니다."),HttpStatus.OK);
+        }
+        //이 사용자가 작성한 모든 리뷰들 가져오기 -> 거기서 lecture 컬럼 빼오기
+        List<Review> reviews=reviewService.findAllReviewsByUser(user);
+        if(reviews.isEmpty()){
+            return new ResponseEntity<>(ResponseMessage.withData(200,"리뷰를 남긴 강의가 존재하지 않습니다.",reviews),HttpStatus.OK);
+        }
+        List<RoadmapLecturesResponse> userLectureList=new ArrayList<>();
+        for(Review review:reviews){
+            Lecture lecture=review.getLecture();
+            RoadmapLecturesResponse roadmapLecturesResponse=new RoadmapLecturesResponse();
+            BeanUtils.copyProperties(lecture,roadmapLecturesResponse);
+            roadmapLecturesResponse.setHashTags(lectureService.getBestHashtags(lecture));
+            userLectureList.add(roadmapLecturesResponse);
+        }
+        return new ResponseEntity<>(ResponseMessage.withData(200,"사용자가 등록한 강의 목록입니다.",userLectureList),HttpStatus.OK);
     }
 
 }
