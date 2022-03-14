14b424f9224e1ded48e8c816167e2df2633639a8
somsom13 <somin1113@naver.com>
2022-03-14 12:40:21 +0900

    [ADD] 소속인증 테이블 생성

---
+++ src/main/java/com/example/demo/user/Company.java
@@ -0,0 +1,26 @@
+package com.example.demo.user;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "companyDomains")
+@NoArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companyDomainId;
+
+    @Column
+    private String companyName;
+
+    @Column
+    private String companyDomain;
+    //어떤 도메인이든 상관없이 메일 보낼 수 있는지 찾아보기
+    //숭실대, 네이버는 테스트용으로 집어넣은 것
+
+}
