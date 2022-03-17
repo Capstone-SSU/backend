2f5a9aa6afc2772dbc485ce138149ac929a05b46
Song-EunJu <jd06280@naver.com>
2022-03-17 17:41:16 +0900

    [FEAT] 해시태그 전체 목록 조회

--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>, CustomUserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
     User findByUserEmail(String email);
     User findByUserNickname(String nickname);
     void deleteByUserId(Long userId);
