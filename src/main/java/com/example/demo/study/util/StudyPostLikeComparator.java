0f9095d59419f9563876c9aac5ffe1fd8c571bf8
somsom13 <somin1113@naver.com>
2022-03-26 18:53:19 +0900

    [FIX] 좋아요 수 기준 정렬 내림차순, 오름차순

--- src/main/java/com/example/demo/study/util/StudyPostLikeComparator.java
+++ src/main/java/com/example/demo/study/util/StudyPostLikeComparator.java
@@ -11,16 +11,16 @@ public class StudyPostLikeComparator  implements Comparator<StudyPostLikeCalc> {
     @Override
     public int compare(StudyPostLikeCalc s1, StudyPostLikeCalc s2) {
         if(s1.getLikeCount() < s2.getLikeCount()) {
-            ret = -1;
+            ret = 1;
         }
         else if(s1.getLikeCount() > s2.getLikeCount()) {
-            ret = 1;
+            ret = -1;
         }else{
             if(Objects.equals(s1.getLikeCount(), s2.getLikeCount())) {
                 if(s1.getStudyPost().getStudyPostId()<s2.getStudyPost().getStudyPostId()){
-                    ret=-1;
-                }else{
                     ret=1;
+                }else{
+                    ret=-1;
                 }
             }
         }
