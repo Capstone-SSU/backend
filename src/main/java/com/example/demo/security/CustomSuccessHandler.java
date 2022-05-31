76a116fb69af2f0a4a7608f2d1663d6c94c3de00
somsom13 <somin1113@naver.com>
2022-05-31 19:10:07 +0900

    [FIX] 소셜로그인 redirect uri 수정

--- src/main/java/com/example/demo/security/CustomSuccessHandler.java
+++ src/main/java/com/example/demo/security/CustomSuccessHandler.java
@@ -33,29 +33,32 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         System.out.println("handler called");
-        String targetUrl = determineTargetUrl(request, response, authentication);
+//        String targetUrl = determineTargetUrl(request, response, authentication);
 
-        getRedirectStrategy().sendRedirect(request, response, checkNickname(authentication,request,targetUrl));
+        getRedirectStrategy().sendRedirect(request, response, checkNickname(authentication,request));
     }
 
 
-    private String checkNickname(Authentication authentication,HttpServletRequest request,String targetUrl){
+    private String checkNickname(Authentication authentication,HttpServletRequest request){
         User user=(User)request.getSession().getAttribute("user");
         Long savedUserId=user.getUserId();
         System.out.println("userID: "+savedUserId);
 //        String nodeId=(String)request.getSession().getAttribute("nodeId");
 
         String userNickname=user.getUserNickname();
+        String targetUrl="http://moyeo.org/pickit/";
 
         if(userNickname.contains("_CONFLICT")){
             //중복 닉네임이 있는 유저라는 의미이므로, 새로운 닉네임 폼으로 보내주어야 한다
             return UriComponentsBuilder.fromUriString(targetUrl).path("nickname/{userId}")
                     .build().expand(savedUserId).toUriString();
+            //깃허브 닉네임 충돌 시: pick-it/nickname/userId -> userId 로 사용자 닉네임 수정해서 다시 요청해야함
         }else{
             String token = tokenProvider.generateJwtToken(authentication);
             String res=UriComponentsBuilder.fromUriString(targetUrl).path("github-login/{userId}/{token}")
                     .buildAndExpand(savedUserId,token).toUriString();
             System.out.println(res);
+            //로그인 성공시: github-login/userId/토큰
 //            res=targetUrl+"github-login/"+savedUserId;
             return res;
 
