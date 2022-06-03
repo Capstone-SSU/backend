2d2c3903c9509fbb44146d1fcf725ac7c63a5319
Song-EunJu <jd06280@naver.com>
2022-06-03 22:37:23 +0900

    [Mod] 추천 알고리즘 전송용 메소드 forEach 추가

--- src/main/java/com/example/demo/lecture/dto/AllLecturesForRecommendResponse.java
+++ src/main/java/com/example/demo/lecture/dto/AllLecturesForRecommendResponse.java
@@ -1,5 +1,6 @@
 package com.example.demo.lecture.dto;
 
+import com.example.demo.lecture.Lecture;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,5 +16,14 @@ public class AllLecturesForRecommendResponse {
     private double avgRate; // 별점
     private int reviewCnt;
     private List<String> hashtags; // 해시태그 리스트
+
+    public static AllLecturesForRecommendResponse from(Lecture lecture){
+        return AllLecturesForRecommendResponse.builder()
+                .lectureId(lecture.getLectureId())
+                .lectureTitle(lecture.getLectureTitle())
+                .avgRate(lecture.getAvgRate())
+                .reviewCnt(lecture.getReviews().size())
+                .build();
+    }
 }
 // [’강의 번호’,’강의 제목’,’평점’,’리뷰를 한 사용자의 수’,’키워드(해시)’]
